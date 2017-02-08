e350d17c77c088de7512b9cc13321eea0d09840f
bruno <bruno.marty4@gmail.com>
2017-02-08 15:20:06 +0100

    initial commit

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -40,6 +40,20 @@ class ComposerAutoloaderInit1d9f8c72becf320e93d6be866cf97047
 
         $loader->register(true);
 
+        $includeFiles = require __DIR__ . '/autoload_files.php';
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire1d9f8c72becf320e93d6be866cf97047($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire1d9f8c72becf320e93d6be866cf97047($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
